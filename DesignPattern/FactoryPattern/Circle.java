765b311d6f3df53eae436b277e206fd4456498a1
wetwc198892 <103273791@qq.com>
2020-11-12 14:45:39 -0800

    1

---
+++ DesignPattern/FactoryPattern/Circle.java
@@ -0,0 +1,11 @@
+package DesignPattern;
+
+public class Circle implements Shape{
+
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+        System.out.println("Inside Circle::draw() method.");
+    }
+    
+}
---
+++ DesignPattern/FactoryPattern/Shape.java
@@ -0,0 +1,5 @@
+package DesignPattern;
+
+public interface Shape{
+    void draw();
+}
